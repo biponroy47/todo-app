4e79a280c96688b90aaad2f1d0b89777d3479206
Bipon Roy <biponroy47@yahoo.com>
2023-05-29 13:04:22 -0400

    defined todo schema and model using mongoose

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+//define todo schema and model using Mongoose
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
